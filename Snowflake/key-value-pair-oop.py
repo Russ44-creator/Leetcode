6bd324dd2f1c50e81a49f7b3fee11046fa12e5e3
Russ44-creator <russ1254016017@gmail.com>
2024-02-05 21:25:56 -0500

    add more categories

---
+++ Snowflake/key-value-pair-oop.py
@@ -0,0 +1,15 @@
+'''
+write a data structure (key-String, value-Integer pair; unique keys, 
+might have duplicate values)
+Ex: ("a", 1), ("b", 2), ("c", 1)
+实现以下方法
+1. insert a new key, value pair
+2. Given a key, return the value
+3. Given a value, return a String[] of keys
+4. Return the key with the smallest value (can be any key，
+比如例子里最小的为1，return a/c都行），delete the corresponding key, value pair
+'''
+
+hashmap = {}
+valueList = {} 
+# or a heap
