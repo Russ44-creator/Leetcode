e640927efb96f0f30b1dd0f42318bc107139a73f
Russ44-creator <russ1254016017@gmail.com>
2024-09-21 17:36:03 -0700

    add more codes

--- Stripe/http-request.py
+++ Stripe/http-request.py
@@ -2,64 +2,164 @@
 # https://www.1point3acres.com/bbs/thread-1080140-1-1.html
 '''
 Part 1
-In an HTTP request, the Accept-Language header describes the list of
-languages that the requester would like content to be returned in. The header
-takes the form of a comma-separated list of language tags. For example:
-Accept-Language: en-US, fr-CA, fr-FR
+In an HTTP request, the Accept-Language header describes the list of languages that the requester would like content to be returned in.
+The header takes the form of a comma-separated list of language tags. For example:
+  Accept-Language: en-US, fr-CA, fr-FR
 means that the reader would accept:
-1. English as spoken in the United States (most preferred)
-2. French as spoken in Canada
-3. French as spoken in France (least preferred)
-We're writing a server that needs to return content in an acceptable language
-for the requester, and we want to make use of this header. Our server doesn't
-support every possible language that might be requested (yet!), but there is a
-set of languages that we do support. Write a function that receives two arguments:
-an Accept-Language header value as a string and a set of supported languages,
-and returns the list of language tags that will work for the request. The
-language tags should be returned in descending order of preference (the
-same order as they appeared in the header).
-In addition to writing this function, you should use tests to demonstrate that it's
-correct, either via an existing testing system or one you create.
+  1. English as spoken in the United States (most preferred)
+  2. French as spoken in Canada
+  3. French as spoken in France (least preferred)
+
+We're writing a server that needs to return content in an acceptable language for the requester, and we want to 
+make use of this header.
+Our server doesn't support every possible language that might be requested (yet!), but there is a set of languages 
+that we do support.
+
+Write a function that receives two arguments: an Accept-Language header value as a string and a set of supported languages,
+and returns the list of language tags that will work for the request. 
+The language tags should be returned in descending order of preference (the same order as they appeared in the header).
+
+In addition to writing this function, you should use tests to demonstrate that it's correct, 
+either via an existing testing system or one you create.
+
 Examples:
-parseacceptlanguage(
-"en-US, fr-CA, fr-FR", # the client's Accept-Language header, a string
-["fr-FR", "en-US"] # the server's supported languages, a set of strings
-)
-returns: ["en-US", "fr-FR"]
-parseacceptlanguage("fr-CA, fr-FR", ["en-US", "fr-FR"])
-returns: ["fr-FR"]
-parseacceptlanguage("en-US", ["en-US", "fr-CA"])
-returns: ["en-US"]***
+parse_accept_language(
+  "en-US, fr-CA, fr-FR",  # the client's Accept-Language header, a string
+  ["fr-FR", "en-US"]      # the server's supported languages, a set of strings
+) returns: ["en-US", "fr-FR"]
+
+parse_accept_language("fr-CA, fr-FR", ["en-US", "fr-FR"]) returns: ["fr-FR"]
+
+parse_accept_language("en-US", ["en-US", "fr-CA"]) returns: ["en-US"]
+
 '''
+clientsAcceptLanguage = "en-US, fr-CA, fr-FR"
+serversSupportedLanguages = ["en-US", "fr-CA", "fr-FR"]
+
+def parse_accept_language(clientsAcceptLanguage, serversSupportedLanguages):
+    clients_list = clientsAcceptLanguage.split(", ")
+    res = []
+    for i in range(len(clients_list)):
+        if clients_list[i] in serversSupportedLanguages:
+            res.append(clients_list[i])
+    return res
+print(parse_accept_language("fr-CA, fr-FR", ["en-US", "fr-FR"]))
+
 '''
-part 2 是问如果client可以直接specify language需要怎么去修改第一轮的答案。example:
-parseacceptlanguage(
-"en, fr-CA, fr-FR", # the client's Accept-Language header, a string
-["fr-FR", "en-US"] # the server's supported languages, a set of strings
-) -> ["en-US", "fr-CA", "fr-FR"]
-注意这里要按照client preference。
-part 3 是问如果client可以有 "*"来表示client takes any language。part 3挺简单的。没时间做part 4但还是过了。
+Part 2
+Accept-Language headers will often also include a language tag that is not region-specific - 
+for example, a tag of "en" means "any variant of English".
+Extend your function to support these language tags by letting them match all specific variants of the language.
+
+Examples:
+parse_accept_language("en", ["en-US", "fr-CA", "fr-FR"]) returns: ["en-US"]
+
+parse_accept_language("fr", ["en-US", "fr-CA", "fr-FR"]) returns: ["fr-CA", "fr-FR"]
+
+parse_accept_language("fr-FR, fr", ["en-US", "fr-CA", "fr-FR"]) returns: ["fr-FR", "fr-CA"]
+'''
+from collections import defaultdict
+
+def parse_accept_language_part2(clientsAcceptLanguage, serversSupportedLanguages):
+    server_dict = defaultdict(list)
+    res = set()
+    for server_language in serversSupportedLanguages:
+        language, area = server_language.split("-")
+        server_dict[language].append(area)
+    for clients_language in clientsAcceptLanguage.split(", "):
+        if "-"  in clients_language:
+            if clients_language in serversSupportedLanguages:
+                res.add(clients_language)
+        else:
+            for area in server_dict.get(clients_language, []):
+                res.add(clients_language + "-" + area)
+    return list(res)
+
+clientsAcceptLanguage = "fr-FR, fr"
+serversSupportedLanguages = ["en-US", "fr-CA", "fr-FR"]
+print(parse_accept_language_part2("fr-FR, fr", ["en-US", "fr-CA", "fr-FR"]))
+
+'''
+Part 3
+Accept-Language headers will sometimes include a "wildcard" entry, represented by an asterisk, 
+which means "all other languages".
+Extend your function to support the wildcard entry.
+
+Examples:
+parse_accept_language("en-US, *", ["en-US", "fr-CA", "fr-FR"]) returns: ["en-US", "fr-CA", "fr-FR"]
+
+parse_accept_language("fr-FR, fr, *", ["en-US", "fr-CA", "fr-FR"]) returns: ["fr-FR", "fr-CA", "en-US"]
 '''
 
-def parseAccepteLanguage(accept_language_header: str, supported_languages: list[str]) -> list[str]:
-    supported_language_map: dict[str, set[str]] = {}
-    for language in supported_languages:
-        lang, country = language.split("-")
-        if lang not in supported_language_map:
-            supported_language_map[lang] = set()
+def parse_accept_language_part3(clientsAcceptLanguage, serversSupportedLanguages):
+    server_dict = defaultdict(list)
+    res = set()
+    for server_language in serversSupportedLanguages:
+        language, area = server_language.split("-")
+        server_dict[language].append(area)
+    for clients_language in clientsAcceptLanguage.split(", "):
+        if clients_language == "*":
+            return serversSupportedLanguages
+        if "-"  in clients_language:
+            if clients_language in serversSupportedLanguages:
+                res.add(clients_language)
+        else:
+            for area in server_dict.get(clients_language, []):
+                res.add(clients_language + "-" + area)
+    return list(res)
 
-        supported_language_map[lang].add(country)
+clientsAcceptLanguage = "fr-FR, fr, *"
+serversSupportedLanguages = ["en-US", "fr-CA", "fr-FR"]
+print(parse_accept_language_part3("fr-FR, fr, *", ["en-US", "fr-CA", "fr-FR"]))
 
-    results = []
-    for language in accept_language_header.split(" "):
-        lang, country = language.split("-")
+'''
+Part 4
+Accept-Language headers will sometimes include explicit numeric weights (known as q-factors) for their entries,
+which are used to designate certain language tags as specifically undesired. For example:
+  Accept-Language: fr-FR;q=1, fr;q=0.5, fr-CA;q=0
+This means that the reader most prefers French as spoken in France, will take any variant of French after that,
+but specifically wants French as spoken in Canada only as a last resort.
 
-        if lang not in supported_language_map:
-            continue
+Extend your function to parse and respect q-factors.
 
-        if country not in supported_language_map[lang]:
-            continue
+Examples:
+parse_accept_language("fr-FR;q=1, fr-CA;q=0, fr;q=0.5", ["fr-FR", "fr-CA", "fr-BG"]) 
+returns: ["fr-FR", "fr-BG", "fr-CA"]
 
-        results.append(language)
+parse_accept_language("fr-FR;q=1, fr-CA;q=0, *;q=0.5", ["fr-FR", "fr-CA", "fr-BG", "en-US"]) 
+returns: ["fr-FR", "fr-BG", "en-US", "fr-CA"]
 
-    return results
+parse_accept_language("fr-FR;q=1, fr-CA;q=0.8, *;q=0.5", ["fr-FR", "fr-CA", "fr-BG", "en-US"])
+'''
+
+def parse_accept_language_part4(clientsAcceptLanguage, serversSupportedLanguages):
+    server_dict = defaultdict(list)
+    res = defaultdict(list)
+    for server_language in serversSupportedLanguages:
+        language, area = server_language.split("-")
+        server_dict[language].append(area)
+    already_add = set()
+
+    for language_with_weight in clientsAcceptLanguage.split(", "):
+        language, weight = language_with_weight.split(";")
+        weight = float(weight.split("=")[1])
+        if language == "*":
+            for server_language in serversSupportedLanguages:
+                if server_language not in already_add:
+                    res[weight].append(server_language)
+        elif "-"  in language:
+            if language in serversSupportedLanguages:
+                res[weight].append(language)
+        else:
+            for area in server_dict.get(language, []):
+                if language + "-" + area not in already_add:
+                    res[weight].append(language + "-" + area)
+    ans = []
+    for key, value in sorted(res.items(), reverse=True):
+        for val in value:
+            ans.append(val)
+    return ans
+
+clientsAcceptLanguage = "fr-FR;q=1, fr-CA;q=0.8, *;q=0.5"
+serversSupportedLanguages = ["fr-FR", "fr-CA", "fr-BG", "en-US"]
+print(parse_accept_language_part4("fr-FR;q=1, fr-CA;q=0.8, *;q=0.5", ["fr-FR", "fr-CA", "fr-BG", "en-US"]))
--- Stripe/mutual_rank.py
+++ Stripe/mutual_rank.py
@@ -55,15 +55,93 @@ if b's second choice becomes their first choice, no mutually-ranked pairings are
 changed_pairings('b', 1) # returns []
 '''
 
+def has_mutual_first_choice(username, wishlists):
+    # Call has_mutual_pair_for_rank() with rank 0, which checks for mutual first choices
+    return has_mutual_pair_for_rank(username, wishlists, 0)
+
+# Function to check if two users are mutually ranked at a specific rank
+def has_mutual_pair_for_rank(username, wishlists, rank):
+    # Get the first choice of the given user at the specified rank
+    user_wishlist = wishlists.get(username)
+    
+    if not user_wishlist or len(user_wishlist) <= rank:
+        return False  # Return False if the rank is out of bounds
+    
+    other_user = user_wishlist[rank]
+    
+    # Get the wishlist of the other user
+    other_user_wishlist = wishlists.get(other_user)
+    
+    if not other_user_wishlist or len(other_user_wishlist) <= rank:
+        return [] # Return False if the rank is out of bounds for the other user
+    
+    # Check if both users have each other at the same rank in their respective wishlists
+    if other_user_wishlist[rank] == username:
+        return [other_user]
+    else:
+        return []
+
+def changed_pairings(username, wishlists, rank):
+    # Get the current user wishlist
+    user_wishlist = wishlists.get(username)
+    
+    if not user_wishlist or len(user_wishlist) <= rank:
+        return []  # Return an empty list if the rank is out of bounds
+    
+    # Check the current pairings for the given user at the rank
+    affected_users = []
+    
+    # Check if incrementing the rank affects mutual pairings
+    for other_user, other_user_wishlist in wishlists.items():
+        if other_user == username:
+            continue
+        # Get mutual pair status before and after the change for both users
+        was_mutual_before = has_mutual_pair_for_rank(username, wishlists, rank)
+        print(was_mutual_before)
+        # Simulate rank change by swapping with the entry above
+        if rank > 0:
+            wishlists[username][rank], wishlists[username][rank - 1] = wishlists[username][rank - 1], wishlists[username][rank]
+        is_mutual_after = has_mutual_pair_for_rank(username, wishlists, rank)
+        print(is_mutual_after)
+        # Swap back to restore the original order
+        if rank > 0:
+            wishlists[username][rank], wishlists[username][rank - 1] = wishlists[username][rank - 1], wishlists[username][rank]
+        
+        # Add the user to affected list if mutual status changes
+        if was_mutual_before != is_mutual_after:
+            affected_users.append(other_user)
+    
+    return affected_users
+
+wishlists = {
+    'a': ['c', 'd'],
+    'b': ['d', 'a', 'c'],
+    'c': ['a', 'b'],
+    'd': ['c', 'a', 'b']
+}
+
+# print(has_mutual_first_choice('a', wishlists))
+# print(has_mutual_pair_for_rank('a', wishlists, 1))
+
+# Example usage:
+print(changed_pairings('d', wishlists, 1))
+# print(changed_pairings('b', wishlists, 2))
+# print(changed_pairings('b', wishlists, 1))
+
+
 '''
 Step 3
 A user's last choice is the last entry on their wishlist. Their second-to-last choice is second to last on their wishlist.
-This can be continued to define third-to-last choice, and so on, always counting from the end of the user's list of apartments.
+This can be continued to define third-to-last choice, and so on, always counting from the end of the user's list of 
+apartments.
 
-A mutually-ranked-anti-pairing is one where both parties rank each other's apartments identically near to (or far from) the *end* of each of their wishlists.
+A mutually-ranked-anti-pairing is one where both parties rank each other's apartments identically near to (or far from) 
+the *end* of each of their wishlists.
 
-Implement changed_antipairings(username, rank) to return an array of the users whose pairings with the given user either gain or lose mutually-ranked-anti-pairing status as a result of the change.
-Note that, as before, the username and rank passed in identify the entry whose rank is being bumped up, so (a, 1) would refer to a's second-choice.
+Implement changed_antipairings(username, rank) to return an array of the users whose pairings with the given user 
+either gain or lose mutually-ranked-anti-pairing status as a result of the change.
+Note that, as before, the username and rank passed in identify the entry whose rank is being bumped up, so (a, 1) 
+would refer to a's second-choice.
 
 if b's third choice becomes their second choice, b and c will no longer be a mutually-ranked anti-pairing
 changed_antipairings('b', 2) # returns ['c']
@@ -71,6 +149,5 @@ changed_antipairings('b', 2) # returns ['c']
 if a's second choice becomes their first choice, a and c will be no longer be a mutually ranked anti-pairing
 in addition, a and d will become a mutually ranked anti-pairing (the second-to-last choice of each other)
 changed_antipairings('a', 1) # returns ['c', 'd']
-
 
 '''
