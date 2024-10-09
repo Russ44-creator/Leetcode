c1c6f0e30c0d5330567d2a713eccef8277da2608
Russ44-creator <russ1254016017@gmail.com>
2024-10-08 23:58:23 -0400

    add more codes

---
+++ WeRide/lc68-Text Justification.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        result = []
+        cur = []
+        num_of_letters = 0
+        for word in words:
+            if num_of_letters + len(word) + len(cur) - 1 >= maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i % (len(cur) - 1 or 1)] += ' '
+                result.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur.append(word)
+            num_of_letters += len(word)
+        return result + [" ".join(cur).ljust(maxWidth)]
